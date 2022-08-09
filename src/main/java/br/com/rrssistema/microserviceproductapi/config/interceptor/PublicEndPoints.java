255dfdeb1a0495d728e6b37f44ec2f2491f2d399
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-08-09 09:17:17 -0300

    Ajustando endpoint publicos

--- src/main/java/br/com/rrssistema/microserviceproductapi/config/interceptor/PublicEndPoints.java
+++ src/main/java/br/com/rrssistema/microserviceproductapi/config/interceptor/PublicEndPoints.java
@@ -8,20 +8,18 @@ import lombok.Getter;
 public enum PublicEndPoints {
 
     FIND_PRODUCT_ALL("/api/product"),
-    FIND_PRODUCT_GETA_ALL("/api/product/all"),
     FIND_PRODUCT_ID("/api/product/{id}"),
-    FIND_PRODUCT_ONE("/api/product/one/{id}"),
-    FIND_PRODUCT_NAME("/api/product/name/{name}"),
-    FIND_PRODUCT_CATEGORY("/api/product//category/{categoryId}"),
-    FIND_PRODUCT_SUPPLIER("/api/product/supplier/{supplierId}"),
+    FIND_PRODUCT_NAME("/api/product/name/"),
+    FIND_PRODUCT_CATEGORY("/api/product/category/"),
+    FIND_PRODUCT_SUPPLIER("/api/product/supplier/"),
 
     FIND_CATEGORY_ALL("/api/category"),
-    FIND_CATEGORY_ONE("/api/category/{id}"),
-    FIND_CATEGORY_NAME("/api/category/description/{description}"),
+    FIND_CATEGORY_ONE("/api/category/"),
+    FIND_CATEGORY_NAME("/api/category/description/"),
 
     FIND_SUPPLIER_ALL("/api/supplier"),
-    FIND_SUPPLIER_ONE("/api/supplier/{id}"),
-    FIND_SUPPLIER_NAME("/api/supplier/name/{name}");
+    FIND_SUPPLIER_ONE("/api/supplier/"),
+    FIND_SUPPLIER_NAME("/api/supplier/name/");
 
     private final String publicEndPoint;
 }
